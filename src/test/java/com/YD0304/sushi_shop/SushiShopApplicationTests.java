edd92aa8218ae337af9040dbb2f0e8729483db27
Youwei Dong <ydong@livebarn.com>
2026-06-16 09:21:33 -0400

    Initial commit

---
+++ src/test/java/com/YD0304/sushi_shop/SushiShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.YD0304.sushi_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SushiShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
